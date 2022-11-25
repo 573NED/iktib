643882ccb00115a4eade4ba4d5225589fcc1b464
573NED <573ned@gmail.com>
2022-11-26 01:12:38 +0300

    _

--- loc/main.py
+++ loc/main.py
@@ -1,8 +1,8 @@
-latitude1 = 47.20168089915109
-longitude1 = 38.93606726189317
+latitude1 = 47.201680
+longitude1 = 38.936067
 
-latitude2 = 47.203218079988375
-longitude2 = 38.9337863042296
+latitude2 = 47.203218
+longitude2 = 38.933786
 
 user_latitude = float(input("latitude :"))
 user_longitude = float(input ("longitude :"))
