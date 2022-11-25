47c6b9584e86bf8848a9dd20acba5d0356e26878
573NED <573ned@gmail.com>
2022-11-26 00:33:24 +0300

    рабочий диапазон

--- loc/main.py
+++ loc/main.py
@@ -1,3 +1,13 @@
-f = open('list.txt', 'r+')
-print(*f)
-f.close()
+latitude1 = 47.20168089915109
+longitude1 = 38.93606726189317
+
+latitude2 = 47.203218079988375
+longitude2 = 38.9337863042296
+
+user_latitude = float(input("latitude :"))
+user_longitude = float(input ("longitude :"))
+
+if latitude2 > user_latitude > latitude1 and longitude2 < user_longitude < longitude1:
+    print("все ништяк")
+else:
+    print("ты где мужик")
